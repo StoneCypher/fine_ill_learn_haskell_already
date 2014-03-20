d5bed308577c90e0dfd64087a6a594e113df6b67
John Haugeland <stonecypher@gmail.com>
2014-03-20 01:23:52 -0700

    damn you reed richards, euler 2

--- euler.hs
+++ euler.hs
@@ -1,2 +1,5 @@
 
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
 euler_1 = sum [ i | i <- [1..999], (((mod i 5) == 0) || ((mod i 3) == 0)) ]
+euler_2 = sum . filter even $ takeWhile (< 4000000) fibs
